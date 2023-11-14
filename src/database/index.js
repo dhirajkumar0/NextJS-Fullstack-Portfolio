a73b02303bdf917390383aa2987f951db7fc371a
Dhiraj <kumardhiraj259@gmail.com>
2023-11-14 20:45:08 +0530

    mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async function connectToDB() {
   try {
     await mongoose.connect(
-      "mongodb+srv://sangam:sangam2023@cluster0.7aauitj.mongodb.net/"
+      "mongodb+srv://dhiraj:1SfPoYwCXCT7ll2r@cluster0.qkwdwrb.mongodb.net/"
     );
     console.log("Database connected successfully");
   } catch (e) {
